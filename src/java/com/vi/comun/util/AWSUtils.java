3faa07aebee7ca289dbb91a4a42a1c84b18e8f32
jerviver21 <jerviver21@localhost.localdomain>
2014-03-15 17:03:05 -0500

    AWS capacidad!

---
+++ src/java/com/vi/comun/util/AWSUtils.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.vi.comun.util;
+
+import com.amazonaws.auth.PropertiesCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import java.io.IOException;
+
+/**
+ *
+ * @author jerviver21
+ */
+public class AWSUtils {
+    
+    public static AmazonS3 getAmazonS3()throws IOException{
+        PropertiesCredentials prop = new PropertiesCredentials(AWSUtils.class.getResourceAsStream("AwsCredentials.properties"));
+        System.out.println("--->* "+prop.getAWSAccessKeyId()+" *<------");
+        AmazonS3 s3client = new AmazonS3Client(new PropertiesCredentials(AWSUtils.class.getResourceAsStream("AwsCredentials.properties")));
+        return s3client;
+    }
+    
+}
